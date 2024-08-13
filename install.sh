b33c7686df6eb609aa507ad65dc5d52412a282b6
mfahad960 <muhfahad960@gmail.com>
2024-08-13 18:21:05 +0500

    updated packages

--- install.sh
+++ install.sh
@@ -31,32 +31,32 @@ echo "adding timeshift service"
 sudo systemctl enable --now cronie
 
 #sym links for dotfiles
-ln -s ~/Repositories/arch_kde/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -s ~/Repositories/arch_kde/.config/hypr/hyprpicker.conf ~/.config/hypr/hyprpicker.conf
-ln -s ~/Repositories/arch_kde/.config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -s ~/Repositories/catppuccin-hyprland/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -s ~/Repositories/catppuccin-hyprland/.config/hypr/hyprpicker.conf ~/.config/hypr/hyprpicker.conf
+ln -s ~/Repositories/catppuccin-hyprland/.config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 
-ln -s ~/Repositories/arch_kde/.config/neofetch/config.conf ~/.config/neofetch/config.conf
+ln -s ~/Repositories/catppuccin-hyprland/.config/neofetch/config.conf ~/.config/neofetch/config.conf
 
-ln -s ~/Repositories/arch_kde/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+ln -s ~/Repositories/catppuccin-hyprland/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 
-ln -s ~/Repositories/arch_kde/.config/starship/starship.toml ~/.config/starship.toml
+ln -s ~/Repositories/catppuccin-hyprland/.config/starship/starship.toml ~/.config/starship.toml
 
-ln -s ~/Repositories/arch_kde/.config/wlogout/assets ~/.config/wlogout/assets
-ln -s ~/Repositories/arch_kde/.config/wlogout/style.css ~/.config/wlogout/style.css
-ln -s ~/Repositories/arch_kde/.config/wlogout/layout ~/.config/wlogout/layout
+ln -s ~/Repositories/catppuccin-hyprland/.config/wlogout/assets ~/.config/wlogout/assets
+ln -s ~/Repositories/catppuccin-hyprland/.config/wlogout/style.css ~/.config/wlogout/style.css
+ln -s ~/Repositories/catppuccin-hyprland/.config/wlogout/layout ~/.config/wlogout/layout
 
-ln -s ~/Repositories/arch_kde/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/Repositories/arch_kde/.config/kitty/theme.conf ~/.config/kitty/theme.conf
+ln -s ~/Repositories/catppuccin-hyprland/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/Repositories/catppuccin-hyprland/.config/kitty/theme.conf ~/.config/kitty/theme.conf
 
-ln -s ~/Repositories/arch_kde/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
-ln -s ~/Repositories/arch_kde/.config/waybar/style.css ~/.config/waybar/style.css
+ln -s ~/Repositories/catppuccin-hyprland/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -s ~/Repositories/catppuccin-hyprland/.config/waybar/style.css ~/.config/waybar/style.css
 
-ln -s ~/Repositories/arch_kde/.bashrc ~/.bashrc
+ln -s ~/Repositories/catppuccin-hyprland/.bashrc ~/.bashrc
 
-ln -s ~/Repositories/arch_kde/.config/scripts/memory.sh ~/.config/scripts/memory.sh
-ln -s ~/Repositories/arch_kde/.config/scripts/logout.sh ~/.config/scripts/logout.sh
-ln -s ~/Repositories/arch_kde/.config/scripts/rofi.sh ~/.config/scripts/rofi.sh
-ln -s ~/Repositories/arch_kde/.config/scripts/mediaplayer.py ~/.config/scripts/mediaplayer.py
+ln -s ~/Repositories/catppuccin-hyprland/.config/scripts/memory.sh ~/.config/scripts/memory.sh
+ln -s ~/Repositories/catppuccin-hyprland/.config/scripts/logout.sh ~/.config/scripts/logout.sh
+ln -s ~/Repositories/catppuccin-hyprland/.config/scripts/rofi.sh ~/.config/scripts/rofi.sh
+ln -s ~/Repositories/catppuccin-hyprland/.config/scripts/mediaplayer.py ~/.config/scripts/mediaplayer.py
 
 #install fonts
 yay -S ttf-work-sans
