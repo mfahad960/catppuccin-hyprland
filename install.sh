5ce6e808054eeef7af548eb1623f0f4e9e24e9ee
Muhammad Fahad <muhfahad960@gmail.com>
2024-11-01 18:53:32 +0500

    updates

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir ~/Repositories
 cd ~/Repositories
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -sfi
 echo "yay installed: "
 yay --version
 echo "installing aur packages..."
@@ -17,13 +17,11 @@ yay -S --needed - < aur_pkgs.txt
 
 #setup firewall
 echo "setting up firewall..."
-systemctl start ufw
-systemctl enable ufw
 
 ufw default allow outgoing
 ufw default deny incoming
-
 ufw allow 22/tcp
+enable ufw
 ufw status
 
 #additional startup services
@@ -31,32 +29,38 @@ echo "adding power profiles service"
 sudo systemctl enable power-profiles-daemon.service
 echo "adding timeshift service"
 sudo systemctl enable --now cronie
-
+echo "adding bluetooth service"
+sudo systemctl enable --now bluetooth
 #sym links for dotfiles
-ln -s ~/Repositories/catppuccin-hyprland/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -s ~/Repositories/catppuccin-hyprland/.config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
-ln -s ~/Repositories/catppuccin-hyprland/.config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
+ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 
-ln -s ~/Repositories/catppuccin-hyprland/.bashrc ~/.bashrc
-ln -s ~/Repositories/catppuccin-hyprland/.config/scripts/ ~/.config/scripts/
+ln -sf ~/Repositories/catppuccin-hyprland/.bashrc ~/.bashrc
 
-ln -s ~/Repositories/catppuccin-hyprland/.config/rofi ~/.config/rofi
-ln -s ~/Repositories/catppuccin-hyprland/.config/dunst ~/.config/
+ln -sf ~/Repositories/catppuccin-hyprland/.config/scripts/ ~/.config
 
-ln -s ~/Repositories/catppuccin-hyprland/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
-ln -s ~/Repositories/catppuccin-hyprland/.config/waybar/style2.css ~/.config/waybar/style.css
+ln -sf ~/Repositories/catppuccin-hyprland/.config/rofi ~/.config/rofi
+ln -sf ~/Repositories/catppuccin-hyprland/.config/dunst ~/.config/dunst
 
-ln -s ~/Repositories/catppuccin-hyprland/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/Repositories/catppuccin-hyprland/.config/kitty/theme.conf ~/.config/kitty/theme.conf
+mkdir ~/.config/waybar
+ln -sf ~/Repositories/catppuccin-hyprland/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -sf ~/Repositories/catppuccin-hyprland/.config/waybar/style2.css ~/.config/waybar/style.css
+ln -sf ~/Repositories/catppuccin-hyprland/catppuccin/waybar/mocha.css ~/.config/waybar/mocha.css
 
-ln -s ~/Repositories/catppuccin-hyprland/.config/neofetch/config.conf ~/.config/neofetch/config.conf
+sudo rm ~/.config/kitty
+ln -sf ~/Repositories/catppuccin-hyprland/.config/kitty ~/.config/kitty
+
+ln -sf ~/Repositories/catppuccin-hyprland/.config/neofetch ~/.config/neofetch
+
 mkdir ~/.config/fastfetch
-ln -s ~/Repositories/catppuccin-hyprland/.config/fastfetch/full.jsonc ~/.config/fastfetch/config.jsonc
+ln -sf ~/Repositories/catppuccin-hyprland/.config/fastfetch/full.jsonc ~/.config/fastfetch/config.jsonc
 
-ln -s ~/Repositories/catppuccin-hyprland/.config/starship/starship.toml ~/.config/starship.toml
+ln -sf ~/Repositories/catppuccin-hyprland/.config/starship/starship.toml ~/.config/starship.toml
 
-ln -s ~/Repositories/catppuccin-hyprland/.config/wlogout/ ~/.config/wlogout/
+ln -sf ~/Repositories/catppuccin-hyprland/.config/wlogout ~/.config/wlogout
 
 echo "done!"
-echo "rebooting..."
+echo "press any key to reboot..."
+read -n 1 -s
 reboot
