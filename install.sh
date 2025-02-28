d5384285209675b41dab2f727fe52bb60d3f64e3
Muhammad Fahad <muhfahad960@gmail.com>
2025-02-28 12:36:01 +0500

    changes to install script

--- install.sh
+++ install.sh
@@ -33,42 +33,38 @@ echo "adding bluetooth service"
 sudo systemctl enable --now bluetooth
 
 #set cursor
+echo "setting cursor..."
 echo 'gtk-cursor-theme-name="Bibata-Modern-Ice"' >> ~/.gtkrc-2.0
 echo -e "[Settings]\ngtk-cursor-theme-name=Bibata-Modern-Ice" >> ~/.config/gtk-3.0/settings.ini
 
 #brave persistent flag
+echo "setting brave persistent flag..."
 echo "--password-store=basic" >> ~/.config/brave-flags.conf
 
 #sym links for dotfiles
-ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
-ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+echo "setting up dotfiles..."
 
+ln -sf ~/Repositories/catppuccin-hyprland/.config/hypr/ ~/.config/hypr
+ln -sf ~/Repositories/catppuccin-hyprland/.config/scripts ~/.config/scripts
 ln -sf ~/Repositories/catppuccin-hyprland/.bashrc ~/.bashrc
-
-ln -sf ~/Repositories/catppuccin-hyprland/.config/scripts/ ~/.config
-
 ln -sf ~/Repositories/catppuccin-hyprland/.config/rofi ~/.config/rofi
 ln -sf ~/Repositories/catppuccin-hyprland/.config/dunst ~/.config/dunst
 
-mkdir ~/.config/waybar
+mkdir -p ~/.config/waybar
 ln -sf ~/Repositories/catppuccin-hyprland/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc
 ln -sf ~/Repositories/catppuccin-hyprland/.config/waybar/style2.css ~/.config/waybar/style.css
 ln -sf ~/Repositories/catppuccin-hyprland/catppuccin/waybar/mocha.css ~/.config/waybar/mocha.css
 
-sudo rm ~/.config/kitty
 ln -sf ~/Repositories/catppuccin-hyprland/.config/kitty ~/.config/kitty
-
 ln -sf ~/Repositories/catppuccin-hyprland/.config/neofetch ~/.config/neofetch
 
-mkdir ~/.config/fastfetch
+mkdir -p ~/.config/fastfetch
 ln -sf ~/Repositories/catppuccin-hyprland/.config/fastfetch/full.jsonc ~/.config/fastfetch/config.jsonc
 
 ln -sf ~/Repositories/catppuccin-hyprland/.config/starship/starship.toml ~/.config/starship.toml
-
 ln -sf ~/Repositories/catppuccin-hyprland/.config/wlogout ~/.config/wlogout
 
-echo "done!"
+echo "All done!"
 echo "press any key to reboot..."
 read -n 1 -s
 echo "rebooting..."
